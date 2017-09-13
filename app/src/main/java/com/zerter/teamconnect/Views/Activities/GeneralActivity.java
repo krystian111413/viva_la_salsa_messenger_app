e3c903ee0a1ae8e67e9f52895b6d7041a3d27010
Kystian Kowalski <krystian111413@gmail.com>
2017-09-13 14:30:43 +0200

    pre-merge

--- app/src/main/java/com/zerter/teamconnect/Views/Activities/GeneralActivity.java
+++ app/src/main/java/com/zerter/teamconnect/Views/Activities/GeneralActivity.java
@@ -4,9 +4,6 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,11 +12,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import com.zerter.teamconnect.Views.Fragments.MenageGroupContacts;
 import com.zerter.teamconnect.EdytujGrupyFragment;
-import com.zerter.teamconnect.Views.Fragments.Message;
 import com.zerter.teamconnect.R;
+import com.zerter.teamconnect.Views.Fragments.MenageGroupContacts;
+import com.zerter.teamconnect.Views.Fragments.Message;
 
 public class GeneralActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
